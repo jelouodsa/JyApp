752bf0c3029fe1386ae639323a060e69dd2ecd13
jelouodsa <jelouodsa@gmail.com>
2015-07-07 00:25:29 +0530

    mysql database configured

--- webserver.go
+++ webserver.go
@@ -5,9 +5,17 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
-	"github.com/jelouodsa/JyApp/createuser"
+	"database/sql"
+  _ "github.com/go-sql-driver/mysql"
 )
 
+var (
+	db *sql.DB
+)
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "hello, world Melvinodsa")
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
@@ -16,32 +24,38 @@ func index(w http.ResponseWriter, r *http.Request) {
 	y := strings.Index(m,"&")
 	username := m[x+1:y]
 	ls := m[y+1:]
-	
+
 	x = strings.Index(ls,"=")
 	y = strings.Index(ls,"&")
 	email := ls[x+1:y]
 	ts := strings.Index(email,"%40")
 	email = email[:ts]+"@"+email[ts+3:]
-	
+
 	m = ls[y+1:]
 	x = strings.Index(m,"=")
 	password:= m[x+1:]
-	
-	_, errcre := createuser.CreateNewuser(username,email,password)
-	if errcre != nil {
-		fmt.Println(errcre)
-		fmt.Fprintf(w, errcre.Error())
+
+	_, err := db.Query("INSERT INTO userlist VALUES( ?, ?, ? )", username, email, password )
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, err.Error())
 	} else{
 		fmt.Println(username)
 		fmt.Println(email)
 		fmt.Println(password)
 		fmt.Fprintf(w, "User created")
 	}
-	
 }
 
 
+
 func main() {
+	http.HandleFunc("/", hello)
 	http.HandleFunc("/registeruser", index);
 	http.ListenAndServe(":8080", nil)
+	db, err = sql.Open("mysql", "root:q@tcp(localhost:3306)/candlelight")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
